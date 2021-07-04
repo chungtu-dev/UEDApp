a3dfd8480bdcc214f00b59c49a9b016a6bfbd1b5
Chung Tú <53727903+chungtu-dev@users.noreply.github.com>
2021-07-04 20:57:31 +0700

    Update Setup.js

--- Setup.js
+++ Setup.js
@@ -5,14 +5,14 @@ import Auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaj6ViUiGQuIrU_kXmPGa8mlC1myiRQ-U",
-    authDomain: "uedapp-41d7b.firebaseapp.com",
-    databaseURL: "https://uedapp-41d7b.firebaseio.com",
-    projectId: "uedapp-41d7b",
-    storageBucket: "uedapp-41d7b.appspot.com",
-    messagingSenderId: "413395699428",
-    appId: "1:413395699428:web:3f2a6afec31fa1b8860ef1",
-    measurementId: "G-KHY5FKWPVH"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   if(!firebase.apps.length){
